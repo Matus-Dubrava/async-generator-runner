7688cee80c880044f38e9b20965534defedc6d17
Sklep <matusdubrava@gmail.com>
2018-08-07 23:37:40 +0200

    added improved version

---
+++ async-generator-runner-imp.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch');
+
+run(function *gen() {
+  const data = yield fetch('http://localhost:3000');
+  const obj = yield data.json();
+  const value = yield obj.value;
+  console.log(value);
+});
+
+function run(fn) {
+  const iterator = fn();
+  const iteration = iterator.next();
+
+  function iterate(iteration) {
+   const promise = Promise.resolve(iteration.value);
+   if (iteration.done) { return iteration.value; }
+   promise.then((v) => { iterate(iterator.next(v)); });
+  }
+
+  iterate(iteration);
+}
